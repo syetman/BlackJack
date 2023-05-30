27ca4ecba9caca45dedb6a28600ee50e73a4a99d
Shaun Yetman <57810567+syetman@users.noreply.github.com>
2023-05-30 12:20:42 -0400

    commit message

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let firstCard = 11
+let firstCard = 10
 let secondCard = 11
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
-if (sum < 21){
-    console.log("do you want another card?")
-} else if(sum === 21){
-    console.log('BLACKJACK!')
+let message = ""
+
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
 } else {
-    console.log("game over")
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
+
+console.log(message)
